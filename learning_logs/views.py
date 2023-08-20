63fd14f32858614e58e7d9bfb0f73be7f79f124f
hharpreetk <harpreetk2120@gmail.com>
2023-08-20 12:14:26 -0400

    restricted topics and entry access to appropriate users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,28 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+
+
+def check_topic_owner(request, topic):
+    """Checks if topic belongs to the current owner"""
+    if topic.owner != request.user:
+        raise Http404
 
 
 def index(request):
     """The home page for Learning Log"""
     return render(request, "learning_logs/index.html")
 
+
 @login_required
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
+
 
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user
+    check_topic_owner(request, topic)
+
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
 
 @login_required
 def new_topic(request):
@@ -34,6 +47,8 @@ def new_topic(request):
         # POST data submitted, process the data
         form = TopicForm(data=request.POST)
         if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
             form.save()
             return redirect("learning_logs:topics")
 
@@ -41,10 +56,13 @@ def new_topic(request):
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
 
+
 @login_required
 def edit_topic(request, topic_id):
     """Edit an existing topic"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
+
     if request.method != "POST":
         # Initial request, prefill form with the current topic
         form = TopicForm(instance=topic)
@@ -58,10 +76,12 @@ def edit_topic(request, topic_id):
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/edit_topic.html", context)
 
+
 @login_required
 def delete_topic(request, topic_id):
     """Delete a topic and its associated entries."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
 
     if request.method == "POST":
         # Delete the topic and its entries
@@ -71,10 +91,12 @@ def delete_topic(request, topic_id):
     context = {"topic": topic}
     return render(request, "learning_logs/delete_topic.html", context)
 
+
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for particular topic"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
 
     if request.method != "POST":
         # No data submitted, create a blank form
@@ -92,11 +114,13 @@ def new_entry(request, topic_id):
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
 
+
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
 
     if request.method != "POST":
         # Initial request, prefill form with the current entry
@@ -111,12 +135,14 @@ def edit_entry(request, entry_id):
     context = {"entry": entry, "topic": topic, "form": form}
     return render(request, "learning_logs/edit_entry.html", context)
 
+
 @login_required
 def delete_entry(request, entry_id):
     """Delete an entry"""
     # Retrieve entry to be deleted
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
 
     if request.method == "POST":
         # Check if the request method is a POST (confirming deletion)
